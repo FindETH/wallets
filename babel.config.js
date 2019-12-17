0906034d09fca19177f6eac4e257aa30808a40e1
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2019-12-17 22:57:47 +0100

    Use Babel for transpiling & update dependencies

--- babel.config.js
+++ babel.config.js
@@ -1,27 +1,48 @@
-const IS_TEST = process.env.NODE_ENV === 'test';
-
 module.exports = {
   "presets": [
     "@babel/preset-typescript",
     [
       "@babel/preset-env",
       {
-        "useBuiltIns": "entry",
-        "corejs": 3,
+        "modules": false,
         "targets": {
-          "node": "12"
+          "node": "current"
         }
       }
     ]
   ],
   "plugins": [
+    "@babel/plugin-proposal-class-properties",
     "@babel/plugin-proposal-nullish-coalescing-operator",
     "@babel/plugin-proposal-optional-chaining",
     [
       "@babel/plugin-transform-runtime",
       {
-        "useESModules": !IS_TEST
+        "useESModules": true
       }
     ]
-  ]
+  ],
+  "env": {
+    "test": {
+      "presets": [
+        [
+          "@babel/preset-env",
+          {
+            "modules": "commonjs",
+            "targets": {
+              "node": "current"
+            }
+          }
+        ]
+      ],
+      "plugins": [
+        [
+          "@babel/plugin-transform-runtime",
+          {
+            "useESModules": false
+          }
+        ]
+      ]
+    }
+  }
 };
