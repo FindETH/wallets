33dd1552e93fe2ed10741e534f4640abfe86af20
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2021-03-09 15:59:23 +0100

    Re-export functions from @findeth/hdnode (#218)
    * Re-export functions from @findeth/hdnode
    
    * Export ExtendedKeyVersion type

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,3 +2,6 @@ export * from './derivation-paths';
 export * from './ledger';
 export * from './memoize';
 export * from './transports';
+
+export { isValidMnemonic, isExtendedKey, getExtendedKeyVersion } from '@findeth/hdnode';
+export type { ExtendedKeyVersion } from '@findeth/hdnode';
