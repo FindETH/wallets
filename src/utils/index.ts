cdcea34291e18234e15a41b393a168dcb815eb95
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2021-03-09 15:56:45 +0100

    Re-export functions from @findeth/hdnode

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,3 +2,5 @@ export * from './derivation-paths';
 export * from './ledger';
 export * from './memoize';
 export * from './transports';
+
+export { isValidMnemonic, isExtendedKey, getExtendedKeyVersion } from '@findeth/hdnode';
