73b84e5c74d944dec3869df47f7825f2ba0333c5
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2021-03-09 15:57:54 +0100

    Export ExtendedKeyVersion type

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -4,3 +4,4 @@ export * from './memoize';
 export * from './transports';
 
 export { isValidMnemonic, isExtendedKey, getExtendedKeyVersion } from '@findeth/hdnode';
+export type { ExtendedKeyVersion } from '@findeth/hdnode';
